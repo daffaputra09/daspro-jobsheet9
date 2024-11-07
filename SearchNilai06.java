2bf7de1b4f998758172d6f7a8684da8d72c89884
daffaputra09 <daffaputra863@gmail.com>
2024-11-07 07:41:54 +0700

    modify SearchNilai06

--- SearchNilai06.java
+++ SearchNilai06.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class SearchNilai06 {
     public static void main(String[] args) {
-        int[] arrNilai = {80,85,78,96,90,82,86};
-        int key = 90;
-        int hasil = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = input.nextInt();
+        int[] arrNilai = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
+            arrNilai[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = input.nextInt();
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
@@ -10,8 +23,11 @@ public class SearchNilai06 {
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di index ke-" + hasil);
-        System.out.println();
+
+        if (hasil != -1) {
+            System.out.println("\nNilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil+1) + "\n");
+        } else {
+            System.out.println("\nNilai yang dicari tidak ditemukan\n");
+        }
     }
 }
